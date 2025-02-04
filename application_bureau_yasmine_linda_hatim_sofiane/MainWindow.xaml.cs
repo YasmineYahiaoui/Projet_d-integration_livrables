fdad4843be667132f1961c64ec457ccc2ebb371b
YasmineYahiaoui <yahiaoui.yasmine11@gmail.com>
2025-02-04 10:49:29 -0500

    Ajout de la fenêtre principale (Main Window) et de la page d'accueil (Home Page)
    Ajout de la fenêtre principale (Main Window) et de la page d'accueil (Home Page),

--- application_bureau_yasmine_linda_hatim_sofiane/MainWindow.xaml.cs
+++ application_bureau_yasmine_linda_hatim_sofiane/MainWindow.xaml.cs
@@ -15,14 +15,72 @@ using System.Windows.Shapes;
 
 namespace application_bureau_yasmine_linda_hatim_sofiane
 {
+
+    public class User
+    {
+        public string userName { get; set; }
+        public string password { get; set; }
+        public string role { get; set; }
+
+        public User(string userName, string password, string role)
+        {
+            this.userName = userName;
+            this.password = password;
+            this.role = role;
+        }
+    }
+
+
     /// <summary>
-    /// Logique d'interaction pour MainWindow.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<User> users = new List<User>()
+        {
+            new User("admin", "mdp123", "admin"),
+            new User("medecin", "mdc123", "med")
+        };
+
+        private User findUser()
+        {
+            return users.Find(user => user.userName == txtUser.Text && user.password == txtPassword.Password);
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Connecter_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtUser.Text) || string.IsNullOrEmpty(txtPassword.Password))
+            {
+                infos.Content = "Veuillez remplir tous les champs.";
+            }
+            else
+            {
+                // Chercher l'utilisateur dans la liste
+                User user = findUser();
+
+                if (user != null)
+                {
+                    // Si l'utilisateur existe, ouvrir la page d'accueil
+                    HomePage homePage = new HomePage(user);
+                    this.Close();
+                    homePage.Show();
+                }
+                else
+                {
+                    // Sinon, afficher un message d'erreur
+                    infos.Content = "Nom d'utilisateur ou mot de passe incorrect. username or password";
+                }
+            }
+        }
+
+        private void login_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
